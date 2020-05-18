71fb3e31f541a23276795fc2ba27263203bb05ad
Brian Smith <brian.e.smith@gmail.com>
2020-05-17 22:57:33 -0700

    Initial import of titanic project using cookiecutter-data-science

---
+++ titanic/README.md
@@ -0,0 +1,5 @@
+Titanic
+=======
+
+Kaggle Titanic competition
+
---
+++ titanic/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='titanic',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Kaggle Titanic competition',
+    author='Brian Smith <brian.e.smith@gmail.com>',
+    license='MIT',
+)
